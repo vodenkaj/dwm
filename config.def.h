2257f3265759f57fcfd78f8f62c99e15322c8036
vodenkaj <vodenkaj3@gmail.com>
2024-12-20 14:13:01 +0100

    feat: new binds

--- config.def.h
+++ config.def.h
@@ -65,14 +65,14 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	// { MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+//	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+//	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	//{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	// { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	//{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+//	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	// { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	// { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -87,8 +87,8 @@ static const Key keys[] = {
 	// { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_bracketright,        spawn,         SHCMD("i3lock") },
   { 0,                            XK_Print, spawn,            SHCMD("flameshot gui") },
-	TAGKEYS(                        XK_ampersand,                      0)
-	TAGKEYS(                        XK_bracketleft,                      1)
+	TAGKEYS(                        XK_0,                      0)
+	TAGKEYS(                        XK_1,                      1)
 	TAGKEYS(                        XK_braceleft,                      2)
 	TAGKEYS(                        XK_braceright,                      3)
 	TAGKEYS(                        XK_parenleft,                      4)
